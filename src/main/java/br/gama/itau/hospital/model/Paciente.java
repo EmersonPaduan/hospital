be052564d595cd1e533e6e4513a10da7461b08ea
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-10 12:05:31 -0300

    fix: Paciente Model

--- src/main/java/br/gama/itau/hospital/model/Paciente.java
+++ src/main/java/br/gama/itau/hospital/model/Paciente.java
@@ -1,5 +1,6 @@
 package br.gama.itau.hospital.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +16,12 @@ public class Paciente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(length = 200)
     private String name;
+
+    @Column(length = 15)
     private String cpf;
+    
+    @Column(length = 15)
     private String fone;
 }
