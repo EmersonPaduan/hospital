6ee8cd03550a2ce14176014afdec3035e1f46cd0
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-10 14:22:16 -0300

    feat: relacionamento

--- src/main/java/br/gama/itau/hospital/model/Paciente.java
+++ src/main/java/br/gama/itau/hospital/model/Paciente.java
@@ -1,10 +1,15 @@
 package br.gama.itau.hospital.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +29,13 @@ public class Paciente {
     
     @Column(length = 15)
     private String fone;
+
+    // Um paciente tem vários atendimentos
+    // mappedBy indica o atributo da relação na tabela Atendimento
+    @OneToMany(mappedBy = "paciente")
+    // Ao preencher os dados dos atendimentos
+    // não preencha os dados do paciente destes atendimentos
+    @JsonIgnoreProperties("paciente")
+    private List<Atendimento> atendimentos;
+
 }
