c6cb2a3540b5a5fe8535b59a87383dbc93b4784c
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-10 17:15:06 -0300

    feat: PacienteServiceTest

--- src/main/java/br/gama/itau/hospital/model/Paciente.java
+++ src/main/java/br/gama/itau/hospital/model/Paciente.java
@@ -10,12 +10,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
